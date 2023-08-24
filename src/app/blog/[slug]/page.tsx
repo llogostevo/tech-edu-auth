528532bc2ccdcfe3a2663d47f21c36251ed8a336
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-08-24 16:06:50 +0100

    blog page styling

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { notFound } from 'next/navigation'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const dynamic = 'force-dynamic'
 
@@ -47,13 +48,29 @@ export default async function BlogPost({ params }: { params: { slug: string } })
                     className='rounded-md' // This will give slightly rounded corners
                 />
                 <p className="text-gray-700 mt-4">{post.content}</p>  {/* Added mt-4 for a slight space between image and content */}
-                <div className="mb-10">
-                <p className="mt-2">{new Date(post.created_at).toLocaleDateString('en-US', {
-                    year: 'numeric', month: 'long', day: 'numeric'
-                })}</p>
-                {/* @ts-ignore */}
-                <p className="mt-2">{`${post.profiles?.first_name.charAt(0)} ${post.profiles?.last_name} `}</p>
+                <div className="mb-4">
+                    <p className="mt-2">{new Date(post.created_at).toLocaleDateString('en-US', {
+                        year: 'numeric', month: 'long', day: 'numeric'
+                    })}</p>
+                    {/* @ts-ignore */}
+                    <p className="mt-2">{`${post.profiles?.first_name.charAt(0)} ${post.profiles?.last_name} `}</p>
                 </div>
+
+                <Link className="py-2 px-4 mb-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group" href="/blog">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+                    >
+                        <polyline points="15 18 9 12 15 6" />
+                    </svg>{' '}Return to BlogBytes</Link>
             </div>
 
             {/* @ts-ignore */}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -28,8 +28,8 @@ export default async function ServerComponent() {
   return <section className="grid md:grid-cols-3 lg:grid-cols-4 gap-8">
   {/* Blog Card */}
   {blogs?.map(blog => (
-    <div key={blog.blog_id} className="bg-white flex flex-col gap-2 p-6 rounded-lg shadow-lg mb-4"> {/* added mb-4 for some spacing between blog cards */}
-      <h2 className="text-3xl font-semibold mb-4">{blog.title}</h2>
+    <div key={blog.blog_id} className="bg-white flex flex-col gap-2 p-6 justify-between rounded-lg shadow-lg mb-4"> {/* added mb-4 for some spacing between blog cards */}
+      <h2 className=" sm:text-lg md:text-xl lg:text-xl font-semibold mb-4">{blog.title}</h2>
       {/* <p>{blog.img_path}</p> */}
       {/* <p className="text-gray-700">{blog.content}</p> */}
       <p>{new Date(blog.created_at).toLocaleDateString('en-US', {
@@ -37,7 +37,7 @@ export default async function ServerComponent() {
       })}</p>
       {/* @ts-ignore */}
       <p>{`${blog.profiles?.first_name.charAt(0)} ${blog.profiles?.last_name} `}</p>  
-      <Link href={`./blog/${blog.blog_id}`}>Visit Blog</Link>
+      <Link href={`./blog/${blog.blog_id}`} className="inline-block border border-primaryColor hover:bg-secondaryColor hover:text-white hover:border-white text-primaryColor rounded px-4 py-2 transition duration-200">Visit Blog</Link>
     </div>
   ))}
 
